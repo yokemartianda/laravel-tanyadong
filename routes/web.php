b3a2a4d3307d77595ef335e26c1938cdd5880b74
yokemartianda <yokemartianda@gmail.com>
2019-06-28 01:58:34 +0700

    Implement favorite question

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::resource('questions.answers', 'AnswersController')->except(['show, index,
 Route::get('/question/{slug}', 'QuestionsController@show')->name('questions.show');
 
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
+
+Route::post('/questions/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
+
+Route::delete('/questions/{question}/favorites', 'FavoritesController@destroy')->name('questions.unfavorite');
