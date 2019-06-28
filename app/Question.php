100bc7ca2f1d8f4b0eb73870c0541c9cd37970a9
yokemartianda <yokemartianda@gmail.com>
2019-06-28 15:11:10 +0700

    Implement question vote functionality

--- app/Question.php
+++ app/Question.php
@@ -77,4 +77,19 @@ class Question extends Model
     {
         return $this->favorites->count();
     }
+
+    public function votes()
+    {
+        return $this->morphToMany(User::class, 'votables');
+    }
+
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
+    }
+
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -56,4 +56,34 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Question::class, 'favorites')->withTimestamps();
     }
+
+    public function voteQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'votables');
+    }
+
+    public function voteAnswers()
+    {
+        return $this->morphedByMany(Answer::class, 'votables');
+    }
+
+    public function questionVote(Question $question, $vote)
+    {
+        $voteQuestions = $this->voteQuestions();
+        if ($voteQuestions->where('votables_id', $question->id)->exists())
+        {
+            $voteQuestions->updateExistingPivot($question, ['vote' => $vote ]);
+        }
+        else
+        {
+            $voteQuestions->attach($question, ['vote' => $vote]);
+        }
+
+        $question->load('votes');
+        $downVotes = (int) $question->downVotes()->sum('vote');
+        $upVotes = (int) $question->upVotes()->sum('vote');
+
+        $question->votes_count = $upVotes + $downVotes;
+        $question->save();
+    }
 }
