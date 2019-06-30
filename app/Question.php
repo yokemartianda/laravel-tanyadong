89effc2edcd7896a6d9e1ccfcb3e99bd164a4fc0
yokemartianda <yokemartianda@gmail.com>
2019-06-30 08:47:11 +0700

    update security feature

--- app/Question.php
+++ app/Question.php
@@ -46,7 +46,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -78,5 +78,20 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return str_limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -53,7 +53,7 @@
                                     <a href="{{$question->user->url}}">{{$question->user->name}}</a>
                                     <small class="text-muted">{{$question->created_date}}</small>
                                 </p>
-                                {{str_limit($question->body, 250)}}
+                                {{ $question->excerpt(300) }}
                             </div>
                         </div>
                         <hr>
